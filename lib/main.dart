62f3aa849cacc66d398c06e36378a65e44809660
Enes Kafa <eneskafa20@gmail.com>
2025-05-19 01:14:05 +0300

    Revert "Added Firebase configuration"
    This reverts commit a1053cbfbdf4e959a021fd38e7266e601fa4e3a6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'homescreen.dart';
 import 'flashcarddecks.dart';
 import 'flashcardQnA.dart';
@@ -11,11 +9,7 @@ import 'login_signup_screen.dart';
 import 'profile_page.dart';
 import 'spaced_repetition.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -27,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Melius Focus',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
+      initialRoute: '/login', // or '/' if you prefer to land on Home directly
       routes: {
         '/login': (context) => const LoginSignupScreen(),
         '/': (context) => const HomeScreen(),
